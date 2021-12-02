97d78ed328773b2dbffda2e8ceca2c4f8e71b7e2
patyspro <patyspro@gmail.com>
2021-12-02 20:19:29 -0300

    add alterações nos produtos com figuras e R$

--- ecommerce/src/components/Home/Home.js
+++ ecommerce/src/components/Home/Home.js
@@ -21,14 +21,14 @@ function Home() {
             <Carousel.Item>
               <img
                 className="d-block w-100 home__image"
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/VG-2019Dec/Desktop_Tallhero_1500-X-600-BB-jan-rvised-NO-Cashback._CB411961897_.jpg"
+                src="https://res.cloudinary.com/dac23jyqr/image/upload/v1638485715/ecommerce/banner_yfojmd.jpg"
                 alt="slide2"
               />
             </Carousel.Item>
             <Carousel.Item>
               <img
                 className="d-block w-100 home__image"
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Audio/Boat/Boat_Gw_1500x600._CB660810557_.jpg"
+                src="https://res.cloudinary.com/dac23jyqr/image/upload/v1638486266/ecommerce/eletronicos_pw440b.jpg"
                 alt="slide3"
               />
             </Carousel.Item>
@@ -42,67 +42,76 @@ function Home() {
             <Carousel.Item>
               <img
                 className="d-block w-100 home__image"
-                src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/TVs/BAU/catlevel/D20458164_IN_HETV_Category_level_GW_PC_Tollhero_1500x600_en_1._CB660812078_.jpg"
+                src="https://res.cloudinary.com/dac23jyqr/image/upload/v1638486424/ecommerce/programa_dl7bac.jpg"
                 alt="slide5"
               />
             </Carousel.Item>
           </Carousel>
         </div>
-        <section>
-          <Product
-            id="12321381"
-            title="Casio Edifice Analog Blue Dial Men's Watch - EFR-552GL-2AVUDF (EX358)"
-            price={40.0}
-            rating={3}
-            image="https://images-na.ssl-images-amazon.com/images/I/71girhyQcJL._UL1100_.jpg"
-          />
-          <Product
-            id="12321371"
-            title="XBOOM Go PL5 Portable Bluetooth Speaker with Meridian Audio Technology"
-            price={25.99}
-            image="https://www.lg.com/us/images/home-audio/md07500093/gallery/desktop-01.jpg"
-            rating={5}
-          />
-          <Product
-            id="12321391"
-            title="Sketchfab Extra bass Headphones Over The Ear Headset with Deep bass (Red)"
-            price={58.99}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/41rBUEf16jL.jpg"
-          />
-        </section>
-
+    
         <div className="categorias">
-          <h2>Produtos por categorias</h2>
+          <h2>Eletrônicos</h2>
+         
         </div>
 
         <div className="home__row">
 
           <Product
-            id="12321341"
+            id="12321381"
+            title="Iphone 11 Pro Max"
+            price={3499.99}
+            rating={3}
+            image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638384699/ecommerce/iphone_ocklvg.jpg"
+          />
+          <Product
+            id="12321371"
             title="HP 14 8th Gen Intel Core i5 Processor 14-inch Thin and Light Laptop , 14q cs0017TU"
-            price={759.99}
+            price={3679.97}
             image="https://images-na.ssl-images-amazon.com/images/I/71Qz7rl%2B5RL._SL1500_.jpg"
+            rating={5}
+          />
+          <Product
+            id="12321391"
+            title="TCL 108 cm (43 inches) 4K Ultra HD Certified Android Smart LED TV 43P615 "
+            price={2789.67}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/71vw4dDvP4L._SL1500_.jpg"
+          />
+     
+        </div>
+
+        <div className="categorias">
+        <h2>Eletrodomésticos</h2>
+          
+        </div>
+
+        <div className="home__row">
+       
+          <Product
+            id="12321341"
+            title="Geladeira Brastemp Frost Free Inox EFR-821G-1D1"
+            price={3250.69}
+            image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385118/ecommerce/geladeira_lzg5vm.jpg"
             rating={4}
           />
 
           <Product
             id="12321351"
-            title="OPPO A12 (Blue, 3GB RAM, 32GB Storage) With No Cost EMI/Additional"
-            price={34.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/71bp9IpcK-L._SX522_.jpg"
+            title="Liquidificador Mondial EFR-834G-1D1"
+            price={99.99}
+            image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385106/ecommerce/liquidificador_nujfud.jpg"
             rating={5}
           />
           <Product
             id="12321361"
-            title="TCL 108 cm (43 inches) 4K Ultra HD Certified Android Smart LED TV 43P615 "
+            title="Microondas Panassonic EFR-837G-1D1" 
             price={1094.98}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/71vw4dDvP4L._SL1500_.jpg"
+            image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385106/ecommerce/microondas_cl5fdf.jpg"
           />
         </div>
         <div className="categorias">
-          <h2>Os mais vendidos</h2>
+          <h2>Mais Vendidos</h2>
         </div>
 
 
@@ -111,20 +120,19 @@ function Home() {
             <section>
               <section className="carrosel">
                 <Product
-                  id="12321341"
-
-                  price={759.99}
-                  image="https://images-na.ssl-images-amazon.com/images/I/71Qz7rl%2B5RL._SL1500_.jpg"
-                  title="HP 14 8th Gen Intel Core i5 Processor 14-inch Thin and Light Laptop , 14q cs0017TU"
-                  rating={4}
+              id="12321381"
+              title="Iphone 11 Pro Max"
+              price={3499.99}
+              rating={3}
+              image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638384699/ecommerce/iphone_ocklvg.jpg"
                 />
 
                 <Product
-                  id="12321351"
-                  title="OPPO A12 (Blue, 3GB RAM, 32GB Storage) With No Cost EMI/Additional"
-                  price={34.99}
-                  image="https://images-na.ssl-images-amazon.com/images/I/71bp9IpcK-L._SX522_.jpg"
-                  rating={5}
+                   id="12321371"
+                   title="HP 14 8th Gen Intel Core i5 Processor 14-inch Thin and Light Laptop , 14q cs0017TU"
+                   price={3679.97}
+                   image="https://images-na.ssl-images-amazon.com/images/I/71Qz7rl%2B5RL._SL1500_.jpg"
+                   rating={5}
                 />
                 <Product
                   id="12321361"
@@ -139,25 +147,25 @@ function Home() {
           <Carousel.Item>
             <section className="carrosel">
               <Product
-                id="12321381"
-                title="Casio Edifice Analog Blue Dial Men's Watch - EFR-552GL-2AVUDF (EX358)"
-                price={40.0}
-                rating={3}
-                image="https://images-na.ssl-images-amazon.com/images/I/71girhyQcJL._UL1100_.jpg"
+                  id="12321341"
+                  title="Geladeira Brastemp Frost Free Inox EFR-821G-1D1"
+                  price={3250.69}
+                  image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385118/ecommerce/geladeira_lzg5vm.jpg"
+                  rating={4}
               />
               <Product
-                id="12321371"
-                title="XBOOM Go PL5 Portable Bluetooth Speaker with Meridian Audio Technology"
-                price={25.99}
-                image="https://www.lg.com/us/images/home-audio/md07500093/gallery/desktop-01.jpg"
-                rating={5}
+                    id="12321351"
+                    title="Liquidificador Mondial EFR-834G-1D1"
+                    price={99.99}
+                    image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385106/ecommerce/liquidificador_nujfud.jpg"
+                    rating={5}
               />
               <Product
-                id="12321391"
-                title="Sketchfab Extra bass Headphones Over The Ear Headset with Deep bass (Red)"
-                price={58.99}
-                rating={4}
-                image="https://images-na.ssl-images-amazon.com/images/I/41rBUEf16jL.jpg"
+                   id="12321361"
+                   title="Microondas Panassonic EFR-837G-1D1" 
+                   price={1094.98}
+                   rating={4}
+                   image="https://res.cloudinary.com/dac23jyqr/image/upload/v1638385106/ecommerce/microondas_cl5fdf.jpg"
               />
             </section>
 
--- ecommerce/src/components/Produtos/Produtos.js
+++ ecommerce/src/components/Produtos/Produtos.js
@@ -27,7 +27,7 @@ function Product({ id, title, image, price, rating }) {
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>$</small>
+          <small>R$ </small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
