7308d4b56c23a6a04d95bd53f6a7b689207094f6
Samuel Xavier <xavierfsamuel@gmail.com>
2021-12-02 23:23:01 -0300

    Criação da estrutura dos cards de produtos

--- ecommerce/src/pages/Produtos/index.js
+++ ecommerce/src/pages/Produtos/index.js
@@ -1,7 +1,7 @@
-import { useParams, Link } from "react-router";
+// import { useParams, Link } from "react-router";
 import { Helmet } from "react-helmet";
-import { Container, Col, ListGroup, Button } from 'react-bootstrap';
-
+import { Col, Row, Button, Card, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import useAxios from "../../hooks/useAxios";
 
 const Produtos = () => {
@@ -14,21 +14,32 @@ const Produtos = () => {
                 <title>CTD Commerce | Todos os produtos</title>
             </Helmet>
             <section id="produtos">
-                <Container className="my-3 text-center">
-                    {produtos.length !== 0 && (
-                        produtos.map((produto) =>
-                        (
-                            <Col key={produto.id}><p>
-                                {console.log(produto)}
-                                Nome: {produto.titulo}, Preço: {produto.preco}, Desc: {produto.descricao}, {produto.categoria.nome}
-                            </p>
-                            </Col>
-
-                        ))
-                    )}
+                <Container >
+                    <Row xs={1} md={3} className="g-4">
+                        {produtos.length !== 0 && (
+                            produtos.map((produto) =>
+                            (
+                                <Col key={produto.id}>
+                                    <Card >
+                                        <Link to={`/produto/${produto.id}`}>
+                                            <Card.Img variant="top" src={produto.image} />
+                                        </Link>
+                                        <Card.Body>
+                                            <Card.Title >{produto.titulo}</Card.Title>
+                                            <Card.Text style={{height:'6rem'}} title={produto.descricao} > 
+                                            {produto.descricao >= 77 ? produto.descricao : produto.descricao.substring(0,76)+ "..."}
+                                            </Card.Text>
+                                            <Button variant="primary" className="mx-auto">Ver detalhes</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )
+                            )
+                        )}
+                    </Row>
                 </Container>
-
             </section>
+
         </>
     )
 }
