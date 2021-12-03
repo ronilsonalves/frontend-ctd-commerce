8439caa63acc53e781a89bdb3bf959ad6bb430c1
Samuel Xavier <xavierfsamuel@gmail.com>
2021-12-03 16:39:31 -0300

    estilização e definição cards

--- ecommerce/src/pages/Produtos/index.js
+++ ecommerce/src/pages/Produtos/index.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { Col, Row, Button, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAxios from "../../hooks/useAxios";
+import './style.scss';
 
 const Produtos = () => {
     const produtos = useAxios();
@@ -14,22 +15,24 @@ const Produtos = () => {
                 <title>CTD Commerce | Todos os produtos</title>
             </Helmet>
             <section id="produtos">
-                <Container >
-                    <Row xs={1} md={3} className="g-4">
+
+
+                <Container className="container">
+                    <Row xs={1} md={3} className="g-4 row">
                         {produtos.length !== 0 && (
                             produtos.map((produto) =>
                             (
-                                <Col key={produto.id}>
-                                    <Card >
+                                <Col key={produto.id} className="col" >
+                                    <Card className="card-container ">
                                         <Link to={`/produto/${produto.id}`}>
                                             <Card.Img variant="top" src={produto.image} />
                                         </Link>
-                                        <Card.Body>
+                                        <Card.Body className="card-body">
                                             <Card.Title >{produto.titulo}</Card.Title>
-                                            <Card.Text style={{height:'6rem'}} title={produto.descricao} > 
+                                            <Card.Text style={{height:'6.50rem'}} title={produto.descricao} className="card-text"> 
                                             {produto.descricao >= 77 ? produto.descricao : produto.descricao.substring(0,76)+ "..."}
                                             </Card.Text>
-                                            <Button variant="primary" className="mx-auto">Ver detalhes</Button>
+                                            <Button variant="primary" >Ver detalhes</Button>
                                         </Card.Body>
                                     </Card>
                                 </Col>
