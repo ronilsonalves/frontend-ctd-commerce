d2b46c0f35a36c1cf899fdc53935cf63536e6abb
patyspro <patyspro@gmail.com>
2021-12-08 12:22:48 -0300

    add Logo e icones de cartao no footer e header

--- ecommerce/src/components/Footer/Footer.js
+++ ecommerce/src/components/Footer/Footer.js
@@ -1,29 +1,47 @@
 import React from "react";
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs';
+import{FaCcVisa, FaCcPaypal,FaCcApplePay} from 'react-icons/fa';
+import{RiMastercardFill}  from 'react-icons/ri';
+import {SiSamsungpay} from 'react-icons/si';
+import{FaInstagramSquare,FaFacebookSquare,FaTwitterSquare} from 'react-icons/fa';
 import { Link } from 'react-router-dom'
+import logo  from '../img/logo.png';
+import './Footer.scss';
 
-
-
-<p className="d-flex">&copy; 2021 </p>
 
 const Footer = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container className="justify-content-center">
-          <Link to="/" className="navbar-brand">E-COMMERCE</Link>
-          <Nav className="justify-content-center">
+          <Navbar.Brand to="/" className="navbar-brand">
+            <img src={logo} alt="logo" className="navbar-brand" />
+          </Navbar.Brand>
+         
+        
 
 
+          <section className="col-md-4 col-sm-6 col-xs-12" >
+            <Link to="/" className="nav-link text-white">Formas de Pagamento</Link>
+            <Nav.Link href="#footer"><FaCcVisa className="text-white me-6" size={'2rem' }  /></Nav.Link>
+      <Nav.Link href="#footer"><RiMastercardFill className="text-white me-6" size={'2rem' }  /></Nav.Link>
+      <Nav.Link href="#footer"><FaCcPaypal className="text-white me-6"size={'2rem' } /></Nav.Link>
+      <Nav.Link href="#footer"><FaCcApplePay className="text-white me-6" size={'2rem' }/></Nav.Link>
+      <Nav.Link href="#footer"><SiSamsungpay className="text-white me-6" size={'2rem' } /></Nav.Link>
+      </section>
 
-            <Link to="/" className="nav-link">Siga as nossas redes sociais</Link>
-            <Link to="/" className="nav-link"><BsGithub className="icon-footer" /></Link>
-            <Link to="/" className="nav-link"><BsLinkedin className="icon-footer" /></Link>
-            <Link to="/" className="nav-link"><BsTwitter className="icon-footer" /></Link>
+ 
+      <Nav className="d-flex justify-content-end"  >
+      <section className="col-md-4 col-sm-6 col-xs-12" >
+      <Nav.Link href="#footer" className="nav-link text-white">Redes Sociais</Nav.Link>
+      <Nav.Link href="#footer" ><FaInstagramSquare className="text-white me-6" size={'2rem' } /></Nav.Link>
+      <Nav.Link href="#footer"><FaFacebookSquare  className="text-white me-6" size={'2rem' } /></Nav.Link>
+      <Nav.Link href="#footer"><FaTwitterSquare  className="text-white me-6" size={'2rem' } /></Nav.Link>
+      </section>
+      
 
-          </Nav>
-        </Container>
+      </Nav>
+      </Container>
       </Navbar>
 
     </>
@@ -36,7 +54,9 @@ const Footer = () => {
 
 
 
-export default Footer;
+export default Footer;  
+
+
 
 
 
